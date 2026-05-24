db0fca39c51b4b43a0abca2a8b64800630e471d3
Sandro Romaddana <sandro.romaddana@gmail.com>
2026-05-24 20:54:13 +0700

    Simplify splash title markup for mobile

--- src/containers/splashScreen/SplashScreen.js
+++ src/containers/splashScreen/SplashScreen.js
@@ -1,20 +1,21 @@
 import React, {useContext} from "react";
 import "./SplashScreen.css";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
-import {greeting, splashScreen} from "../../portfolio";
+import {splashScreen} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function SplashScreen() {
   const {isDark} = useContext(StyleContext);
   return (
     <div className={isDark ? "dark-mode splash-container" : "splash-container"}>
-      <div className="splash-animation-container">
-        <DisplayLottie animationData={splashScreen.animation} />
-      </div>
-      <div className="splash-title-container">
-        <span className="grey-color"> &lt;</span>
-        <span className="splash-title">{greeting.username}</span>
-        <span className="grey-color">/&gt;</span>
+      <div className="splash-content">
+        <div className="splash-animation-container">
+          <DisplayLottie animationData={splashScreen.animation} />
+        </div>
+        <div className="splash-title-container" aria-label="Sandro Romaddana">
+          <span className="splash-title-line">Sandro</span>
+          <span className="splash-title-line">Romaddana</span>
+        </div>
       </div>
     </div>
   );
